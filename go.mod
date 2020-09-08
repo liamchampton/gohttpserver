5718d795a86086c275ad6e0757ef68db231b9d6a
Liam Hampton <liam.hampton@ibm.com>
2020-09-08 14:50:34 +0100

    inital commit - base code for a http server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gohttpserver
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	// Create the route handler listening on '/'
+	http.HandleFunc("/", home)
+	fmt.Println("Starting server on port 8080")
+
+	// Start the sever
+	http.ListenAndServe(":8080", nil)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	// Assign the 'msg' variable with a string value
+	msg := "Hello, welcome to your app"
+
+	// Write the response to the byte array - Sprintf formats and returns a string without printing it anywhere
+	w.Write([]byte(fmt.Sprintf(msg)))
+}
