693f8727cf63d010c08516a87a53e77cb90508fe
Liam Hampton <liam.hampton@ibm.com>
2020-09-24 15:26:24 +0100

    makeing a change to main.go and main_test.go

--- main_test.go
+++ main_test.go
@@ -14,7 +14,7 @@ func TestHomeRoute(t *testing.T) {
 		Home(response, request)
 
 		got := response.Body.String()
-		want := "This is a toolchain test PR"
+		want := "This is a test for liamstoolchain"
 
 		if got != want {
 			t.Errorf("got %q, want %q", got, want)
